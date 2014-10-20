ce0aed68b8338bebadbdc20fdb80a63f39ab0380
innoc <innocent_akhidenor@yahoo.com>
2014-10-19 23:59:42 -0400

    new version

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Giga
   class Application < Rails::Application
     config.assets.initialize_on_precompile = false
     config.serve_static_assets = true
+    config.assets.precompile += %w( *.css *.js )
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
